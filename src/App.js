34206bf73df7dd1ffa47ed19b1dfac6b111c1800
dFuZer <enzotomassi@gmail.com>
2022-05-30 17:58:03 +0200

    Added minimum height and width to body, added components and images folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Component1 from "./components/Component1.js";
+import reactLogo from "./images/react-logo.png";
+
 export default function App() {
     return (
         <div>
+            <img src={reactLogo} alt="" />
             <h1>Let's code! :)</h1>
+            <Component1 />
         </div>
     );
 }
