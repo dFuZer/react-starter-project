2bb660670ed363ef025f604f115cc69fe6382f93
dFuZer <enzotomassi@gmail.com>
2022-06-01 03:44:06 +0200

    Add footer

---
+++ src/components/GlobalFooter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function GlobalFooter() {
+    return (
+        <footer className="globalFooter">
+            <p>
+                Integration by Enzo TOMASSI, 2022. Design from{" "}
+                <a href="#">Figma</a>
+            </p>
+        </footer>
+    );
+}
