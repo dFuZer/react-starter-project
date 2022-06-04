f48cabf156ecb556cbb80ea16933450d34c2b5b3
dFuZer <enzotomassi@gmail.com>
2022-06-04 05:35:28 +0200

    Add split scss files

---
+++ src/components/ExamplePage.js
@@ -0,0 +1,11 @@
+import reactLogo from "../images/react-logo.png";
+import React from "react";
+
+export default function ExamplePage() {
+    return (
+        <div className="examplePage">
+            <h1>Let's code! :)</h1>
+            <img src={reactLogo} alt="" />
+        </div>
+    );
+}
